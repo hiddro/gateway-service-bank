01ced043d85a0b2207663913520c67280ddc416b
@EduCCor <edd.ckalb@gmail.com>
2021-10-31 00:06:03 -0500

    gateway service

---
+++ src/test/java/gateway/service/bank/GatewayServiceBankApplicationTests.java
@@ -0,0 +1,13 @@
+package gateway.service.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
